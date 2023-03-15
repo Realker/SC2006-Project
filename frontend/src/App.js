acaa3586f2afe3ac7a1c46c1556553e6bfe73508
Realker <30778323+Realker@users.noreply.github.com>
2023-03-15 17:31:05 +0800

    Added Frontend Folder

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Test from './components/Test';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+       <NavBar/>
+      <h1>The best Price Comparison Website</h1>
+      <Test/>
+      </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {useCookies} from 'react-cookie';
+
+function NavBar() {
+
+    const [token, SetToken, removeToken] = useCookies(['mytoken'])
+
+    const logoutBtn = () => {
+      removeToken(['mytoken'])
+  
+    }
+
+    return (
+        <div>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">HDB Finder</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active">Log out</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</div>
+    )
+}
+
+export default NavBar
