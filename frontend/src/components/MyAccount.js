27bbe65189e7fb058a1fc35fd8bf8208d3b1840d
jnsy99 <98268475+jnsy99@users.noreply.github.com>
2023-03-22 00:52:04 +0800

    Update MyAccount.js

--- frontend/src/components/MyAccount.js
+++ frontend/src/components/MyAccount.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import NavBar from './NavBar'
 import HDB from '../images/HDBFAQ.jpg'
+import '../css/FAQ.css'
 const MyAccount = () => {
   return (
     <div class = "FAQBackground">
+      
       <NavBar/>
-
-      <h1>My Account</h1>
-      <h1>Profile Username: </h1>
-      <h1>Profile Email: </h1>
+      <div className='FAQ__bg h1'>
+      <h1>Account Details</h1>
+      
+      <h2>My Username:</h2>
+      <h2>My Email:</h2>
       </div>
-  )
+    </div>)
 }
 
 export default MyAccount
