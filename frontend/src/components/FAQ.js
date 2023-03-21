3698d4a31539514b62bcf370af96f5cfc5e5e66e
jnsy99 <98268475+jnsy99@users.noreply.github.com>
2023-03-21 21:39:50 +0800

    FAQ skeleton
    FAQ Skeleton for background and words up

--- frontend/src/components/FAQ.js
+++ frontend/src/components/FAQ.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import NavBar from './NavBar'
+import HDB from '../images/HDBFAQ.jpg'
+import '../css/FAQ.css'
 const FAQ = () => {
   return (
-    <div>
+    <div class = "FAQBackground">
       <NavBar/>
       <h1>FAQ</h1>
     </div>
@@ -10,3 +12,5 @@ const FAQ = () => {
 }
 
 export default FAQ
+
+
