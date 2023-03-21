2077354baf10d26980967266814db6db28c10e82
jnsy99 <98268475+jnsy99@users.noreply.github.com>
2023-03-21 15:24:59 +0800

    SaveHousesToFavourites added

--- frontend/src/components/SaveHousesToFavourites.js
+++ frontend/src/components/SaveHousesToFavourites.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import '../css/SaveHousesToFavourites.css';
+import { NavLink } from 'react-router-dom';
 
 const SaveHousesToFavourites = () => {
   return (
-    <div>
-
-      <h1>HHIIHIH</h1>
+    <div className='SaveHousesToFavourites__bg'>
+      <div className='SaveHousesToFavourites__content'>
+        <h1>Confirm Save House to Favourites?</h1>
+        <div className='SaveHousesToFavourites__buttons'>
+            <NavLink to="/ExploreHouses" className="">
+                <button className='btn-cancel'>Cancel</button>
+            </NavLink>
+            <NavLink to="/MyActivities" className="">
+            <button className='btn-confirm'>Confirm</button>
+          </NavLink>
+        </div>
+      </div>
     </div>
   )
 }
