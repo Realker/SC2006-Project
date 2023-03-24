44265d19e64a1dbbc7633dd9a611e62fad7196b4
jnsy99 <98268475+jnsy99@users.noreply.github.com>
2023-03-25 00:04:03 +0800

    NavBar UI

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React, {useState,useEffect} from 'react'
 import {useCookies} from 'react-cookie';
 import { NavLink, Link } from 'react-router-dom'
-
 import '../css/NavBar.css';
 const currentTab = ({ isActive }) => {
   return isActive
@@ -17,36 +16,33 @@ export default function NavBar() {
       <div>
           <nav class = "NavBar">
               {/* Logo */}
-              <Link to="/Homepage" className={currentTab}>
+              <Link to="/Homepage" className= "Logo">
                   <p className='Logo'>HDBFinder</p>
               </Link>
 
               {/* Menu links */}
               <ul class = "NavBarLink">
-                  <NavLink to="/ExploreHouses" className={currentTab}>
+                  <NavLink to="/ExploreHouses" className="NavBarLink">
                       
                       <li class = "NavBarLink">Explore</li>
                   </NavLink>
-                  <NavLink to="/FAQ" className={currentTab}>
+                  <NavLink to="/FAQ" className="NavBarLink">
                       
                       <li class = "NavBarLink">FAQ</li>
                   </NavLink>
-                  <NavLink to="/MyActivities" className={currentTab}>
+                  <NavLink to="/MyActivities" className="NavBarLink">
                      
                       <li class = "NavBarLink">My Activities</li></NavLink>
-                  <NavLink to={`/myaccount`} className={currentTab}>
+                  <NavLink to={`/myaccount`} className="NavBarLink">
 
                       <li class = "NavBarLink">My Account</li></NavLink>
               </ul>
 
               {/* TODO : handle log out */}
               {/* Log Out */}
-              <NavLink to="/" className="mt-auto mb-sm-5 py-3 btn btn-primary text-danger gap-3 d-flex align-items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-door-closed" viewBox="0 0 16 16">
-                      <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z" />
-                      <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0z" />
-                  </svg>
-                  <span class = "Logout">Log Out</span>
+              <NavLink to="/" className="Logout">
+                  
+                  <span class = "Logout">Logout</span>
               </NavLink>
           </nav>
       </div>
