e4ddd5c651c207eec8c7f92c1da32880e399d6ae
Realker <30778323+Realker@users.noreply.github.com>
2023-03-26 08:30:09 +0800

    Updatedd Bell Styling

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,7 +5,6 @@ import { NavDropdown } from 'react-bootstrap';
 import '../css/NavBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell, faUser} from '@fortawesome/free-solid-svg-icons';
-import '../css/Testt.css'; //For styling of MyAccount DropDown Menu
 import '../css/FilterByParameters.css'
 
 const currentTab = ({ isActive }) => {
