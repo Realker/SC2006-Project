93212b2ba06bccf45f3e58eb77fd17fcf03c17b3
Realker <30778323+Realker@users.noreply.github.com>
2023-03-28 02:39:15 +0800

    FilterByParam

--- frontend/src/components/FilterByParameters.js
+++ frontend/src/components/FilterByParameters.js
@@ -10,6 +10,17 @@ import { faHome, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 //FBPBackground = FilterByParametersBackGround
 const FilterByParameters = () => {
 
+  const [propertyType, setPropertyType] = useState('');
+  const [bedroomCount, setBedroomCount] = useState('');
+  const [priceRange, setPriceRange] = useState('');
+  const [locationType, setLocationType] = useState('');
+
+  //Testing in progress
+  const search = () => {
+    // Filter search results based on selected filter values
+    // ...
+  };
+
   return (
     <div class = "FBPBackground">
     <NavBar/> 
@@ -18,9 +29,43 @@ const FilterByParameters = () => {
         <a href="/Homepage"><FontAwesomeIcon icon={faHome} className="homeicon"/></a>&nbsp;&nbsp;
         <FontAwesomeIcon icon={faChevronRight} className="arrow-right"/>&nbsp;&nbsp;Check Pricing
       </div>
+
+    <div className="filter-parameters">
+      <select value={propertyType} onChange={(e) => setPropertyType(e.target.value)}>
+        <option value="">Property Type</option>
+        <option value="apartment">Apartment</option>
+        <option value="condo">Condominium</option>
+        <option value="landed">Landed</option>
+        <option value="hdb">HDB</option>
+      </select>
+      <select value={bedroomCount} onChange={(e) => setBedroomCount(e.target.value)}>
+        <option value="">No. of Bedrooms</option>
+        <option value="1 bedroom">1 bedroom</option>
+        <option value="2 bedrooms">2 bedrooms</option>
+        <option value="3 bedrooms">3 bedrooms</option>
+        <option value="4 bedrooms">4 bedrooms</option>
+        <option value="5 bedrooms">5 bedrooms</option>
+      </select>
+      <select value={priceRange} onChange={(e) => setPriceRange(e.target.value)}>
+        <option value="">Any Price</option>
+        <option value="0-500000">0 - 500,000</option>
+        <option value="500000-1000000">500,000 - 1,000,000</option>
+        <option value="1000000-2000000">1,000,000 - 2,000,000</option>
+      </select>
+      <select value={locationType} onChange={(e) => setLocationType(e.target.value)}>
+        <option value="">Location</option>
+        <option value="sengkang">Sengkang</option>
+        <option value="tampines">Tampines</option>
+        <option value="jurong">Jurong</option>
+      </select>
+
+      &nbsp;<button className="filterButton">Search</button> 
+      
+      
     </div>
+  </div>
 
   )
 }
 
-export default FilterByParameters
+export default FilterByParameters;
--- frontend/src/components/Testt.js
+++ frontend/src/components/Testt.js
@@ -1,15 +1,53 @@
-import React, {useState,useEffect} from 'react'
-import '../css/Testt.css';
-import TestBar from './TestBar'
-import '../css/Testt.css';
+//import React, {useState,useEffect} from "react"
+//import NavBar from './NavBar'
+//import '../css/Testt.css';
+//import TestBar from './TestBar'
+//import "antd/dist/antd.css";
+//import axios from "axios";
+//import BootstrapTable from "react-bootstrap-table-next";
+//import paginationFactory from "react-bootstrap-table2-paginator";
+//import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+//import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import * as ReactBootStrap from "react-bootstrap";
 
-export default function Testt() {
-  
- 
+/*
+import { useState } from 'react';
+
+function Testt() {
+  const [condoType, setCondoType] = useState('');
+  const [priceRange, setPriceRange] = useState('');
+  const [bedroomCount, setBedroomCount] = useState('');
+
+  const search = () => {
+    // Filter search results based on selected filter values
+    // ...
+  };
+
   return (
-    <div class= "TesttBackGround">
-        <TestBar/>
-  
+    <div>
+      <input type="text" placeholder="Search" />
+      <select value={condoType} onChange={(e) => setCondoType(e.target.value)}>
+        <option value="">Condo Type</option>
+        <option value="1 bedroom">1 bedroom</option>
+        <option value="2 bedrooms">2 bedrooms</option>
+        <option value="3 bedrooms">3 bedrooms</option>
+      </select>
+      <select value={priceRange} onChange={(e) => setPriceRange(e.target.value)}>
+        <option value="">Any Price</option>
+        <option value="0-500000">0 - 500,000</option>
+        <option value="500000-1000000">500,000 - 1,000,000</option>
+        <option value="1000000-2000000">1,000,000 - 2,000,000</option>
+      </select>
+      <select value={bedroomCount} onChange={(e) => setBedroomCount(e.target.value)}>
+        <option value="">Bedroom</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+      <button onClick={search}>Search</button>
     </div>
   );
 }
+export default Testt;
+*/
