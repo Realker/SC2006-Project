6afc0ec513c725eb232df1cc2ac668985521573d
jnsy99 <98268475+jnsy99@users.noreply.github.com>
2023-03-27 21:48:26 +0800

    DivBar in MyActivities
    DivBar in MyActivities fixed

--- frontend/src/components/MyActivities.js
+++ frontend/src/components/MyActivities.js
@@ -2,13 +2,14 @@ import React from 'react'
 import NavBar from './NavBar'
 import ExploreHousesCard from './ExploreHousesCard';
 import '../css/MyActivities.css';
+import DivisionNavBar from './DivisionNavBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 
 const MyActivities = () => {
   return (
-    <div className='Myactivities__bg'>
+    <><div className='Myactivities__bg'>
       <NavBar/>        
 
       <div className="second-nav">
@@ -22,11 +23,20 @@ const MyActivities = () => {
             <ExploreHousesCard/>
             <ExploreHousesCard/>
           </div>
-
           
-</div>
+        
+    </div>
+    <div className= "DivisionBackGround">
+    <DivisionNavBar />
+  
+    </div>     
+
+    
+</>
+    
     
   )
+  
 }
 
 export default MyActivities
